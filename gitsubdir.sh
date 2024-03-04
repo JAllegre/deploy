eb9f211bdc9fb7b9ccb998ddf9efd25241fc73da
JAllegre <ju.allegre@gmail.com>
2024-03-04 09:59:55 +0100

    improve gitsubdir script

--- gitsubdir.sh
+++ gitsubdir.sh
@@ -2,30 +2,30 @@
 
 #!/bin/bash
 
-case $1 in
-  "pull")
-    gitCommand="pull"
-    ;;
-  "push")
-    gitCommand="push"
-    ;;
-  *)
-    gitCommand="status -s"
-    ;;
-esac
+
 
 subfolders=$(find . -type d)
 # Loop through each subdirectory
 for folder in $subfolders; do
   if [ -d "$folder/.git" ]; then
     cd "$folder"
-    echo -e "\e[34m-------$folder----------------------------------------\e[0m"
-    echo "$folder:"
-    git fetch -q
-    git log HEAD..origin/HEAD --oneline
-    git $gitCommand
+    echo -e "\e[34m--- $folder -------------------\e[0m"
+    case $1 in
+      "pull")
+        git pull
+      ;;
+      "push")
+        git push
+      ;;
+      *)
+        git fetch -q
+        git log HEAD..origin --pretty='format: %C(red)REMOTE%C(reset) %s'
+        git status -s
+    ;;
+    esac
+
     cd ..
-    
+    echo ""
   fi
 done
 
