30dd09da0496682506bc7416c432f6b96284ab78
JAllegre <ju.allegre@gmail.com>
2024-03-04 09:30:25 +0100

    handle upstream changes

--- gitsubdir.sh
+++ gitsubdir.sh
@@ -15,15 +15,17 @@ case $1 in
 esac
 
 subfolders=$(find . -type d)
-echo "----------------------------"
 # Loop through each subdirectory
 for folder in $subfolders; do
   if [ -d "$folder/.git" ]; then
     cd "$folder"
+    echo -e "\e[34m-------$folder----------------------------------------\e[0m"
     echo "$folder:"
+    git fetch -q
+    git log HEAD..origin/HEAD --oneline
     git $gitCommand
     cd ..
-    echo "----------------------------"
+    
   fi
 done
 
